28898810c522a63913114b9678fac69ea4741d98
Emi Brown <emicb55@gmail.com>
2022-07-28 12:54:02 -0500

    Added Annotation Text

--- annotations.js
+++ annotations.js
@@ -5,8 +5,8 @@ function display_annotation_overview() {
   text.innerHTML = "Overview (games released from 1980-2020)"
 
   display_svg_annotation(50, 50, bar_width*3, 300, 10, 350, 10, 
-    "Annotations :)", 
-    "Longer text to show text wrapping",
+    "Top 3 Genres", 
+    "Overall, action, sports, and shooter games sell the best",
     200);
 }
 
@@ -14,17 +14,17 @@ function display_annotation_na() {
   text.innerHTML = "NA"
 
   display_svg_annotation(50, 50, bar_width*3, 300, 10, 350, 10, 
-    "Annotations :)", 
-    "Longer text to show text wrapping",
+    "North America", 
+    "The top 3 genres stay consistent with the global model",
     200);
 }
 
 function display_annotation_eu() {
   text.innerHTML = "EU"
 
-  display_svg_annotation(50+bar_width*3, 50, bar_width*4, 300, 10, 280, 120, 
-    "Annotations :)", 
-    "Longer text to show text wrapping",
+  display_svg_annotation(50+bar_width*3, 50, bar_width*4, 300, 10, 300, 100, 
+    "Europe", 
+    "The middle most popular genres are reversed from the global popularity",
     200);
 }
 
@@ -32,13 +32,13 @@ function display_annotation_jp() {
   text.innerHTML = "JP"
 
   display_svg_annotation(50, 50, bar_width*3, 300, 10, 350, 10, 
-    "Annotations :)", 
-    "Longer text to show text wrapping",
+    "Japan", 
+    "RPGs are much more popular in Japan than in any other region",
     200);
 
-  display_svg_annotation(50+bar_width*11, 50, bar_width, 300, 10, -100, 150, 
-    "Annotations :)", 
-    "Longer text to show text wrapping",
+  display_svg_annotation(50+bar_width*11, 50, bar_width, 300, 10, -100, 140, 
+    "Shooters", 
+    "The 3rd most popular genre globally sells the least",
     200);
 }
 
@@ -46,8 +46,8 @@ function display_annotation_other() {
   text.innerHTML = "Other"
 
   display_svg_annotation(50, 50, bar_width*3, 300, 10, 350, 10, 
-    "Annotations :)", 
-    "Longer text to show text wrapping",
+    "Other Regions", 
+    "The top 3 genres also stay consistent in other regions",
     200);
 }
 
@@ -73,7 +73,7 @@ function display_svg_annotation(x_pos, y_pos, w, h, offset, dx_pos, dy_pos, titl
   }]
 
   const makeAnnotations = d3.annotation()
-    .notePadding(15)
+    .notePadding(10)
     .textWrap(wrap)
     .type(type)
     .annotations(annotations)
